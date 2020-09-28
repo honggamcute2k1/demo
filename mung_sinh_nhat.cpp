9d6532a4e5bfb972279c36384200c4c21261edc3
GamHIT <71882554+GamHIT@users.noreply.github.com>
2020-09-28 23:21:56 +0700

    comment first

---
+++ mung_sinh_nhat.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <windows.h>
+void happy_birthday()
+{
+	printf("   *    *     ***      *****   *****    *       *                                   \n");   Sleep(500);
+	printf("   *    *    *   *     *    *  *    *     *   *            \n");   Sleep(500);
+	printf("   ******   *******    *****   *****        *                   \n "); Sleep(500);
+	printf("  *    *  *       *   *       *            *          \n");  Sleep(500);
+	printf("   *    * *         *  *       *            * \n");  Sleep(500);
+	printf("\n\n\n"); 
+	printf(" *****    *   *****     *********   *    *  *****          ***     *       *   \n");Sleep(500);
+	printf(" *    *   *   *    *        *       *    *  *     *       *   *      *   *    \n ");Sleep(500);
+	printf("*    *   *   *    *        *       *    *  *      *     *     *      ***        \n");Sleep(500);
+	printf(" *****    *   * * *         *       ******  *      *    *********      *         \n");Sleep(500);
+	printf(" *    *   *   *    *        *       *    *  *      *   *         *     *         \n");Sleep(500);
+	printf(" *    *   *   *     *       *       *    *  *     *   *           *    *         \n");Sleep(500);
+	printf(" *****    *   *      *      *       *    *  *****    *             *   *         \n");Sleep(500);
+	printf("\n\n\n\n\n\n");
+	printf("                   *  **    *                                           \n ");Sleep(500);
+	printf("                  **   *  **                                             \n  ");Sleep(500);
+	printf("                   *   *   *                                             \n    ");Sleep(500);
+	printf("                   *   *   *                                             \n    ");Sleep(500);
+	printf("                   *   *   *                **  *         * **          \n   ");Sleep(500); 
+	printf("                 **************                *            *             \n");Sleep(500);
+	printf("                    *            *                 *          *               \n ");Sleep(500);
+	printf("                   *   *        *                   *       *                 \n  ");Sleep(500);
+	printf("                  * *   *    * *                     * * *                   \n   ");Sleep(500);
+	printf("                 *       *    *                                               \n");Sleep(500);
+	printf("                    *************                                             \n");Sleep(500);
+	system("cls");
+}
+int main(){
+	while(true){
+		happy_birthday();
+	}
+	return 0;
+}
+
